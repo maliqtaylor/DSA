67fdbbd73d23e3769f2f181f161057782cf64433
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-13 23:37:51 -0400

    accounts for spaces in reverseArray problem

--- arrays.js
+++ arrays.js
@@ -1,9 +1,15 @@
 function reverseArray(a) {
     let i = a.length - 1;
     let ans = '';
-    while( i >= 0 ){
-        ans += a[i]
-        i --
+    while (i >= 0) {
+        if (i === 0) {
+            ans += a[i]
+        }
+        else {
+            ans += a[i] + ' '
+        }
+        i--
     }
     return ans
 }
+console.log(reverseArray([1, 4, 6, 8]))
