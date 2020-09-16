7f4e0e2597d1548abe5ebd3e6e607dc5162800b2
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-16 11:07:35 -0400

    solves shufflestr

--- shufflestr.js
+++ shufflestr.js
@@ -2,20 +2,28 @@
 // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
 
-const shuffleStr = (s, indecies) => {
+const shuffleStr = (s, indices) => {
     let arrStr = s.split('');
-    // let returnArr = [];
     let indexStre = {};
     for (let i = 0; i < arrStr.length; i++) {
         if (!indexStre[arrStr[i]]) {
-            indexStre[arrStr[i]] = [indecies[i]]
+            indexStre[arrStr[i]] = [indices[i]]
         }
         else {
-            indexStre[arrStr[i]] = [...indexStre[arrStr[i]], indecies[i]]
+            indexStre[arrStr[i]] = [...indexStre[arrStr[i]], indices[i]]
         }
     }
-    return indexStre
-    // return returnArr.join('')
+    let returnStr = '';
+    let i = 0;
+    while (i <= arrStr.length) {
+        for (let letter in indexStre) {
+            if (indexStre[letter].includes(i)) {
+                returnStr += letter
+            }
+        }
+        i++
+    }
+    return returnStr
 };
 
 console.log(shuffleStr('aaiougrt', [4, 0, 2, 6, 7, 3, 1, 5]))
