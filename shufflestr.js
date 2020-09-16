9567b6247fbeac552934f3f08889de9e6642e1e0
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-15 23:35:02 -0400

    initial answer

--- shufflestr.js
+++ shufflestr.js
@@ -1,6 +1,17 @@
 // Given a string s and an integer array indices of the same length.
-
 // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-const shuffleStr = (s, indecies) => {
 
+
+const shuffleStr = (s, indecies) => {
+    Array.prototype.insert = function ( index, item ) {
+        this.splice( index, 0, item );
+    };
+    let arrStr = s.split('');
+    let returnArr = [];
+    for(let i = 0; i < indecies.length; i++){
+        returnArr.insert(indecies[i], arrStr[i])
+    }
+    return returnArr.join('')
 };
+
+console.log(shuffleStr('aaiougrt', [4,0,2,6,7,3,1,5]))
