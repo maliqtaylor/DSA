3334ca4bf9a045f17d1f8182260a9224049e2c36
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-17 23:28:55 -0400

    solves identicalPairs

--- identicalPairs.js
+++ identicalPairs.js
@@ -6,4 +6,16 @@
 // Output: 4
 // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 const numIdenticalPairs = (nums) => {
+    let totalPairs = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let curr = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            let next = nums[j];
+            if (curr === next) {
+                totalPairs += 1
+            }
+        }
+    }
+    return totalPairs
 };
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]))
