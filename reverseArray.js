259c2e81937e0fd33a405240e1d0775197f98adc
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-17 23:51:59 -0400

    solves reverseArray

--- reverseArray.js
+++ reverseArray.js
@@ -1,13 +1,8 @@
 function reverseArray(a) {
     let i = a.length - 1;
-    let ans = '';
+    let ans = [];
     while (i >= 0) {
-        if (i === 0) {
-            ans += a[i]
-        }
-        else {
-            ans += a[i] + ' '
-        }
+        ans.push(a[i])
         i--
     }
     return ans
