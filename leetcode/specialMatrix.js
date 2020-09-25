9327d6e141d3ce8d7b5bb7b67274adf83da917ec
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-25 09:05:50 -0400

    initializes specialMetrix problem

---
+++ leetcode/specialMatrix.js
@@ -0,0 +1,36 @@
+// Given a rows x cols matrix mat, where mat[i][j] is either 0 or 1, return the number of special positions in mat.
+
+// A position (i,j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).
+
+// Input: mat = [[1,0,0],
+//               [0,0,1],
+//               [1,0,0]]
+// Output: 1
+// Explanation: (1,2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
+
+// Input: mat = [[1,0,0],
+//               [0,1,0],
+//               [0,0,1]]
+// Output: 3
+// Explanation: (0,0), (1,1) and (2,2) are special positions.
+
+// Input: mat = [[0,0,0,1],
+//               [1,0,0,0],
+//               [0,1,1,0],
+//               [0,0,0,0]]
+// Output: 2
+
+// Special positions are when a 1 has only 0's in its neighboring columns and rows
+
+const numSpecial = (mat) => {
+    let totalRows = 0;
+    let totalColumns = 0;
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat.length; j++) {
+            console.log(mat[i][j])
+        }
+    }
+};
+numSpecial([[1,0,0],
+            [0,0,1],
+            [1,0,0]])
